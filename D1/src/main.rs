1d1886aa5eb5b550930762ec965a05f79ce5df00
Itrekr <itrekr@oscar.cc>
2023-05-13 20:53:09 +0200

    Solved day 1 of Advent of Code 2021. (For real this time)

--- D1/src/main.rs
+++ D1/src/main.rs
@@ -2,12 +2,20 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 
 fn main() {
-    let result = count();
-    println!("Result is {}",result);
+    let numbers_p1 = get_numbers().unwrap();
+    let result_p1 = count(&numbers_p1);
+    println!("Result part 1 is {}",result_p1);
+    let numbers_p2 = sum_numbers(numbers_p1);
+    let result_p2 = count(&numbers_p2);
+    println!("Result part 2 is {}",result_p2)
 }
 
-/// for x in measurments:
-/// check value > prev_value
+/// for indexnr in lijst
+/// if (indexnr+2) exist:
+///     value = indexnr(cor_val) + (indexnr+1) + (indexnr+2)
+///     if prev value < *value
+///     ...
+
 /// if true: aantal=aantal+1
 /// set prev_value value
 
@@ -22,16 +30,13 @@ fn get_numbers() -> std::io::Result<Vec<u16>> {
         }
     }
 
-    // println!("{:?}", numbers);
-
     Ok(numbers)
 }
 
 /// Returns amount of increasing measurements as a 16bit unsigned integer
-fn count() -> u16 {
+fn count(numbers:&Vec<u16>) -> u16 {
     let mut prev_value = u16::MAX;
     let mut result = 0;
-    let numbers = get_numbers().unwrap();
     for (index,value) in numbers.iter().enumerate() {
         if index == 0 {
             println!("{} (N/A - no previous measurement)",*value);
@@ -49,4 +54,13 @@ fn count() -> u16 {
         prev_value = *value;
     }
     return result;
+}
+
+fn sum_numbers(numbers:Vec<u16>) -> Vec<u16> {
+   let mut result = Vec::new();
+   for index in 0..(numbers.len()-2) {
+       let sum = numbers[index]+numbers[index+1]+numbers[index+2];
+       result.push(sum);
+   }
+   return result;
 }
